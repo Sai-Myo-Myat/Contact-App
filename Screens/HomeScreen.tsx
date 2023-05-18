1bab2226b1b660147438ffc57bb9220f119a863b
saimyomyat <saimyomyat511@gmail.com>
2023-05-18 11:45:56 +0630

    adding  packages

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,4 +1,7 @@
 import { View, Text, Button, TextInput } from "react-native"
+
+// import Icon from "react-native-vector-icons"
+
 import CustomButton from "../Components/CustomButton"
 
 import tw from 'twrnc'
@@ -6,10 +9,10 @@ import tw from 'twrnc'
 const HomeScreen = ({ navigation }) => {
     return (
         <View style={[tw`flex flex-row bg-red-500 h-full items-start `]}>
-            <TextInput placeholder="search contact" style={[tw`border-b border-blue-300 m-2 p-1 w-2/3 `]}/>
+            {/* <Icon name="search"  size={30} /> */}
             <CustomButton title="Add" onPressFun={() => console.log("add to contact")}/>
         </View>
     )   
 }
-    
+
 export default HomeScreen;  
