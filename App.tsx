f73f51004d3f4522e1a398e2c4eb9b56ff40bfa6
saimyomyat <saimyomyat511@gmail.com>
2023-05-19 19:44:23 +0630

    react query set-up

--- App.tsx
+++ App.tsx
@@ -2,19 +2,20 @@ import { StyleSheet, StatusBar, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { QueryClientProvider, QueryClient} from "react-query"
 
 import HomeScreen from './Screens/HomeScreen';
 import Form from './Screens/Form';
 
 import CustomRightHeader from './Components/CustomRightHeader';
-import { BottomSheetContext } from '@gorhom/bottom-sheet/lib/typescript/contexts';
 
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
       <StatusBar barStyle={"default"} />
       <Stack.Navigator initialRouteName='Home' screenOptions={{
         headerStyle: {
@@ -36,6 +37,7 @@ export default function App() {
         <Stack.Screen name='Form' component={Form} />
       </Stack.Navigator>
     </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,22 +1,30 @@
 import { View, Text, Button, TextInput } from "react-native"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { FlashList } from "@shopify/flash-list"
-
+import tw from 'twrnc';
+import { useQuery } from "react-query";
 
 import ContactItem from "../Components/ContactItem"
 
-import tw from 'twrnc';
-
 import { db } from "../db";
 
+interface InputType {
+    name: string,
+    phoneNumber: string,
+    dateOfBirth: Date,
+    remark?: string
+}
 
 const HomeScreen = ({ navigation }) => {
-    const fetchData = () => {
+
+    // const [data,setData] = useState<InputType[]>([])
+
+    const fetchAllContact = () => {
         db.transaction(tx => {
           tx.executeSql('SELECT * FROM contact', [],
-            (_txObj, { rows: { _array } }) => {console.log({data: _array}, "this is from db")},
-            (_txObj, error) => {console.log("Error", error)}
+            (_txObj, { rows: { _array } }) => console.log("data:", _array),
+            (_txObj, error) => console.log("error: ", error)
         )})
       }
     useEffect(
@@ -26,9 +34,19 @@ const HomeScreen = ({ navigation }) => {
                   'CREATE TABLE IF NOT EXISTS contact (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phoneNumber TEXT, dateOfBirth TEXT, remark TEXT)'
                 )
             })
-            fetchData()
         }
     ,[])
+
+    const {isLoading,isError, data, error} = useQuery("fetchAllContact", fetchAllContact)
+
+    if (isLoading) {
+        return (
+            <View style={[tw`bg-[#212A3E] w-full h-full`]}>
+                <Text>Loading ......</Text>
+            </View>
+        )
+    }
+
     return (
         <View style={[tw`bg-[#212A3E] w-full h-full`]}>
             <FlashList  data={
@@ -44,16 +62,29 @@ const HomeScreen = ({ navigation }) => {
                     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
                     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
                 ]   
-                }
+            }
                 renderItem={({item}) => {
                     return (
-                        <ContactItem name={item.name} phoneNumber={item.phoneNumber}/>
+                        <ContactItem name={item?.name} phoneNumber={item.phoneNumber}/>
                     )
                 }} 
                 estimatedItemSize={20}
-        />
+            />
         </View>
     )   
 }
 
 export default HomeScreen;  
+
+// [
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+//     {"name": "mgmg", "phoneNumber": "093522453", "dateOfBirth": "2345-34-34", "remark": "adsfadfadsf"},
+// ]   
